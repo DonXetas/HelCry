e7cc13e6b7b167905201f751fb2993a81407ada8
Paul Ab <paul.abramov@live.de>
2017-04-14 12:17:57 +0200

    Added the Registrator to the GameRules files, so we the GameRules get registrated and they can be used inside the project

--- Code/GameRules/GameRules.cpp
+++ Code/GameRules/GameRules.cpp
@@ -3,3 +3,36 @@
 
 // Include the Header file, which defines which functions and variables are used in the class
 #include "GameRules/GameRules.h"
+
+// We have to register our class so it will be recognized as our gamerules
+// We are inherit from IEntityRegistrator, which gives us the functions to register the class
+class CGameRulesRegistrator : public IEntityRegistrator
+{
+	virtual void Register() override
+	{
+		// Register our class "CGameRules" with the name "GameRules"
+		CGamePlugin::RegisterEntityWithDefaultComponent<CGameRules>("GameRules");
+
+		// Get the content of ConsoleVariable "sv_gamerulesdefault"
+		ICVar* pDefaultGameRulesCVar = gEnv->pConsole->GetCVar("sv_gamerulesdefault");
+
+		// To do this, we need the <IGameRulesSystem.h>
+		// Get the current GameRulesSystem, so we can manipulate it
+		auto* pGameRulesSystem = gEnv->pGameFramework->GetIGameRulesSystem();
+
+		//register our class as the new Gamerules to the content of the ConsoleVariable
+		pGameRulesSystem->RegisterGameRules(pDefaultGameRulesCVar->GetString(), "GameRules", false);
+		pGameRulesSystem->AddGameRulesAlias(pDefaultGameRulesCVar->GetString(), pDefaultGameRulesCVar->GetString());
+	}
+
+	// Can be empty, but it has to be implemented because we are forced to implement it, from the parent
+	// virtual void Unregister() = 0;
+	// "= 0" forces us to implement this function, even if we do not need it
+	virtual void Unregister() override
+	{
+
+	}
+};
+
+// Variable of the registrator, which handles the registration internally
+CGameRulesRegistrator g_gameRulesRegistrator;
--- Code/GameRules/GameRules.h
+++ Code/GameRules/GameRules.h
@@ -1,7 +1,15 @@
 #pragma once
+// Header files surrounded with "" are custom files, we can modify or are our files
+// Header files surrounded with <> are internal files, we can't modify them nor are they our files
 
 // We need StdAfx for CryEngine internal functions, which will be called/used
 #include "StdAfx.h"
+
+// We need this file, to register our classes inside the project, so they are getting compiled and we can use them inside the project
+#include "GamePlugin.h"
+
+// We need this file, for internal functions of the registrationsystem
+#include <IGameRulesSystem.h>
 
 // Define our class "GameRules", with this class we will control all rules in the game
 class CGameRules
