76b5ff9f91bef92afea83dbbcda90a345fe3eb0f
Paul Ab <paul.abramov@live.de>
2017-04-14 11:58:50 +0200

    Added GameRules class, which is going to manage the game procedure, added those files in the CmakeLists, so they are getting compiled with the project

---
+++ Code/GameRules/GameRules.cpp
@@ -0,0 +1,5 @@
+// Not needed, because it is included inside the Header file, but compiler throws an error, going to remove in the future
+#include "StdAfx.h"
+
+// Include the Header file, which defines which functions and variables are used in the class
+#include "GameRules/GameRules.h"
---
+++ Code/GameRules/GameRules.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// We need StdAfx for CryEngine internal functions, which will be called/used
+#include "StdAfx.h"
+
+// Define our class "GameRules", with this class we will control all rules in the game
+class CGameRules
+{
+
+};
